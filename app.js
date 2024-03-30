fc6cf92d2b9ffe47d965fe6171eb04082adcebe9
JcTheSavior <jacobconnell3@gmail.com>
2024-03-30 12:24:10 -0400

    Add steps to follow for creating elements in js.
    Ensure there is an easy to follow path with minimal confusion to start.

---
+++ app.js
@@ -0,0 +1,26 @@
+//-----------------------{Instructions}---------------//
+// ---All elements will be inside of the div "middleSectionContainer"---
+// 1. create function for the onclick property of the button from "midFormForm"
+// 2. create midActionSection top div --> then create either "midActionQuiz" OR "midActionHelper" div, depending on which example user choose from midFormForm
+// 3A. {div.midActionQuiz}, create:
+//      3A1: {h4.midQuizTitle} --> 
+//      3A2: {form.midQuizForm} -->
+//          3A3: {fieldset} -->
+//              3A4: {legend} -->
+//              3A5: {4 inputs; 4 labels}
+//              3A6: button with an .addeventlistener
+// 
+// 3B. {div.midActionHelper} -->
+//      3B1: {h4.midHelperTitle} -->
+//      3B2: {form.midHelperForm} -->
+//          3B3: {fieldset} -->
+//              3B4: {legend} -->
+//              3B5: {4 labels; 4 inputs}
+//              3B6: button with an .addeventlistener
+//
+
+
+// Button from html, will activate when clicked and user has chosen an example
+const generateQuizOrHelper = () => {
+    
+}
